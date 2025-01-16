0406671b5854f94a0124169e7ad3c3ff9c13d8ca
Ritesh Biradar <142141512+RiteshBiradar@users.noreply.github.com>
2025-01-16 04:50:16 +0000

    datatypes and conversions

---
+++ basics/conversiondatatype.js
@@ -0,0 +1,13 @@
+let var1 = "36adc";
+let num = Number(var1);
+
+//returns number
+console.log(typeof num)
+
+//returns NaN not a number
+console.log(num)
+
+//"33"=>33
+//"33abc"=>NaN
+//"null"=>0
+//undefined=>NaN
---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert("hello world") // we are using nodejs not browser 
+
+// console.log("check")
+// console.log(typeof undefined);
+// console.log(typeof null)
+
+//Symbol datatype - Interesting, helps to find out the uniqueness 
+
+// const s1 = Symbol("description")
+// const s2 = Symbol("description")
+// console.log(s1==s2)
+
+// const bigNum = 1231321243412312312141213n;
+// console.log(typeof bigNum) //returns bigint, pehele wo undefined return karna padta tha 
+
+console.log(null>0) //false
+console.log(null>=0) //true
+console.log(null==0) //false
